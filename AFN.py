a9cc42294a5b1ef823dd26e7c92bbed8bebd9401
Ale Gomez <gom20347@uvg.edu.gt>
2022-02-12 10:00:10 -1000

    feat. created AFN

--- AFN.py
+++ AFN.py
@@ -1,16 +1,22 @@
-class Estado:
+class AFN:
     def __init__(self):
-        pass
+        self.estados = []
+        self.transiciones = []
+        self.EI = None
+        self.EF = []
+
+
+class Estado:
+    def __init__(self, id):
+        self.id = id
+        self.transiciones = {}
 
 
 class Transicion:
-    def __init__(self):
-        pass
-
-
-class AFN:
-    def __init__(self):
-        pass
+    def __init__(self, s, eI, eF):
+        self.s = s
+        self.eI = eI
+        self.eF = eF
 
 
 class Construccion:
