6fbb4bfcf149234f905e7fac4978d6a5ad9e5d42
Ale Gomez <gom20347@uvg.edu.gt>
2022-02-12 10:00:10 -0600

    feat. started AFN class

---
+++ AFN.py
@@ -0,0 +1,36 @@
+class Estado:
+    def __init__(self):
+        pass
+
+
+class Transicion:
+    def __init__(self):
+        pass
+
+
+class AFN:
+    def __init__(self):
+        pass
+
+
+class Construccion:
+    def __init__(self):
+        pass
+
+    def Simbolo(self):
+        pass
+
+    def Union(self):
+        pass
+
+    def Concatenacion(self):
+        pass
+
+    def CerraduraKleene(self):
+        pass
+
+    def PosicionKleene(self):
+        pass
+
+    def Interrogacion(self):
+        pass
