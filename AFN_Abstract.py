cda5c157cde4935f152e09578bb00b93be52fefd
Ale Gomez <gom20347@uvg.edu.gt>
2023-02-26 11:39:17 -0600

    feat. commented abstract class

--- AFN_Abstract.py
+++ AFN_Abstract.py
@@ -12,36 +12,45 @@ como buenas practicas de programacion.
 class ConstruccionAbstract(ABC):
     @abstractmethod
     def Transiciones(self, t):
+        # Se define el metodo abstracto para las transiciones
         pass
 
     @abstractmethod
     def ThompsonAlgorithm(self):
+        # Se define el metodo abstracto para el algoritmo de Thompson
         pass
 
     @abstractmethod
     def Thompson(self, i):
+        # Se define el metodo abstracto para el algoritmo de Thompson
         pass
 
     @abstractmethod
     def Simbolo(self, S):
+        # Se define el método abstracto para el simbolo
         pass
 
     @abstractmethod
     def Union(self, AFN, AFN2):
+        # Se define el método abstracto para la union
         pass
 
     @abstractmethod
     def Concatenacion(self, AFN, AFN2):
+        # Se define el método abstracto para la concatenacion
         pass
 
     @abstractmethod
     def CerraduraKleene(self, AFN):
+        # Se define el método abstracto para la cerradura de Kleene
         pass
 
     @abstractmethod
     def CerraduraKleene2(self, AFN):
+        # Se define el método abstracto para la cerradura de Kleene
         pass
 
     @abstractmethod
     def Interrogacion(self, AFN):
+        # Se define el método abstracto para usar el signo de interrogacion
         pass
