48faaf7bd62bc3e94bcecea9870720d370701d56
Ale Gomez <gom20347@uvg.edu.gt>
2023-02-26 15:04:40 -0600

    feat. created abstract class for automata

---
+++ src/Automata.py
@@ -0,0 +1,36 @@
+
+"""
+Definicion de clases abstractas para automata.
+Usado para definir las clases de AFN y AFD
+
+"""
+from abc import ABC, abstractmethod
+
+class Automata(ABC):
+    """
+    Clase abstracta para automata
+    """
+
+    @abstractmethod
+    def __init__(self, estadoI, estadoF, estadosN, transiciones, estados):
+        pass
+
+
+class Transicion(ABC):
+    """
+    Clase abstracta para transicion
+    """
+
+    @abstractmethod
+    def __init__(self, s, eI, eF):
+        pass
+
+
+class Estado(ABC):
+    """
+    Clase abstracta para estado
+    """
+
+    @abstractmethod
+    def __init__(self, id):
+        pass
